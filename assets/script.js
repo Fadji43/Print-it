3005d6034eae9eace81da207c74c8ac34555193e
Fanny <lakrame@hotmail.com>
2023-06-06 13:27:26 +0200

    version2

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,15 @@
+/*const banner = document.getElementById("#banner");
+
+const imageBanner = document.getElementById("banner-img") 
+imageBanner.addEventListener("click", function() {
+console.log("c'est super !"); 
+});
+
+const textBanner = document.getElementById("#banner>p")
+textBanner.addEventListener("click", function() {
+	console.log("et ça ?");  
+});*/
+
 const arrowLeft = document.getElementById("arrow-left");
 arrowLeft.addEventListener("click", function() {
 console.log("ca marche a gauche !"); 
@@ -7,6 +19,7 @@ const arrowRight = document.getElementById("arrow-right");
 arrowRight.addEventListener("click", () => {
 console.log("ca marche a droite!"); 
 });
+
 
 const slides = [ "slide1", "slide2", "slide3", "slide4", 
 	{
@@ -26,41 +39,3 @@ const slides = [ "slide1", "slide2", "slide3", "slide4",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-let numero = 0;
-
-function ChangeSlide(sens) {
-    numero = numero + sens;
-    if (numero < 0)
-        numero = slides.length - 1;
-    if (numero > slides.length - 1)
-        numero = 0;
-    document.getElementById("slides").src = slides[numero];
-	alert(tu as cliqué!);
-}
-
-
-
-/*const imageBanner = document.getElementById("banner-img") 
-imageBanner.addEventListener("click", function() {
-console.log("c'est super !"); 
-});
-
-const textBanner = document.getElementById("#banner>p")
-textBanner.addEventListener("click", function() {
-	console.log("et ça ?");  
-});
-/*function changeSlide (slides)
-for (let i=0; i < slides.length; i++) {
-    const elementSlide = document.getElementById("#banner");
-    const ImageSlide = document.getElementById("banner-img");
-    ImageSlide = slides[i].image;
-    elementSlide.appenchild(ImageElement);
-    document.body.appenchild(elementSlide);
-}*/
-
-
- /*let changeText = document.getElementById("#banner>p") 
- changeText.innerHTML("click", function() =>{
-
- })*/
-
